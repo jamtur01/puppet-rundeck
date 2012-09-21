0cd13448f81446d75e3dcb03797809eba233cecf
James Turnbull <james@lovedthanlost.net>
2012-09-21 07:02:59 -0600

    Updated copyright

--- lib/puppet-rundeck.rb
+++ lib/puppet-rundeck.rb
@@ -1,5 +1,5 @@
 #
-# Copyright 2011, James Turnbull
+# Copyright 2012, James Turnbull
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
